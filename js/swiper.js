174cc3cc072570db8aad2f841d0de70f27484a2d
O.G <alexandrgoya@gmail.com>
2021-07-03 12:18:36 +0300

    products process

--- js/swiper.js
+++ js/swiper.js
@@ -1,8 +1,4 @@
 new Swiper('.main-banner', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    },
     pagination: {
         el: '.swiper-pagination'
     },
@@ -10,42 +6,22 @@ new Swiper('.main-banner', {
     loop: true,
     initialSlide: 1,
 });
-//
-// var Swiper2 = new Swiper('.swiper2', {
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev'
-//     },
-//     pagination: {
-//         el: '.swiper-pagination'
-//     },
-//     slidesPerView: 3,
-//     centeredSlides: true,
-//     initialSlide: 1,
-//     loop: true,
-// });
-//
-// var Swiper3 = new Swiper('.swiper3', {
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev'
-//     },
-//     slidesPerView: 'auto',
-//     initialSlide: 1,
-//     loop: true,
-//
-// });
-//
-// var Swiper4 = new Swiper('.swiper4', {
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev'
-//     },
-//     pagination: {
-//         el: '.swiper-pagination'
-//     },
-//     slidesPerView: 3,
-//     centeredSlides: true,
-//     initialSlide: 1,
-//     loop: true,
-// });
+
+function productImagesSlider() {
+    const products = document.getElementsByClassName('product');
+    for (let i = 0; i < products.length; i++) {
+        const product = products.item(i);
+        new Swiper(product.getElementsByClassName('product-images-slider')[0], {
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev'
+            },
+            pagination: {
+                el: '.swiper-pagination'
+            },
+            slidesPerView: 1,
+            loop: true,
+            initialSlide: 1,
+        });
+    }
+}
