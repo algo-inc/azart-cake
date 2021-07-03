0a9802d029ddb77f97069a4055a588b51fa0c3a5
O.G <alexandrgoya@gmail.com>
2021-07-03 10:31:00 +0300

    very cool main banner

--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,4 @@
-new Swiper('.swiper-container', {
+new Swiper('.main-banner', {
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
@@ -6,46 +6,46 @@ new Swiper('.swiper-container', {
     pagination: {
         el: '.swiper-pagination'
     },
-
-    centeredSlides: true,
-    initialSlide: 1,
-});
-
-var Swiper2 =  new Swiper ('.swiper2', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    },
-    pagination: {
-        el: '.swiper-pagination'
-    },
-    slidesPerView: 3,
-    centeredSlides: true,
-    initialSlide: 1,
+    slidesPerView: 1,
     loop: true,
-});
-
-var Swiper3 =  new Swiper ('.swiper3', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    },
-    slidesPerView: 'auto',
     initialSlide: 1,
-    loop: true,
-
 });
-
-var Swiper4 =  new Swiper ('.swiper4', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    },
-    pagination: {
-        el: '.swiper-pagination'
-    },
-    slidesPerView: 3,
-    centeredSlides: true,
-    initialSlide: 1,
-    loop: true,
-});
+//
+// var Swiper2 = new Swiper('.swiper2', {
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev'
+//     },
+//     pagination: {
+//         el: '.swiper-pagination'
+//     },
+//     slidesPerView: 3,
+//     centeredSlides: true,
+//     initialSlide: 1,
+//     loop: true,
+// });
+//
+// var Swiper3 = new Swiper('.swiper3', {
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev'
+//     },
+//     slidesPerView: 'auto',
+//     initialSlide: 1,
+//     loop: true,
+//
+// });
+//
+// var Swiper4 = new Swiper('.swiper4', {
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev'
+//     },
+//     pagination: {
+//         el: '.swiper-pagination'
+//     },
+//     slidesPerView: 3,
+//     centeredSlides: true,
+//     initialSlide: 1,
+//     loop: true,
+// });
